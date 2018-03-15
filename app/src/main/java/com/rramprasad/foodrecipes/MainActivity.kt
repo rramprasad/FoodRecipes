4c8034d387c7ccc7f4d1fd914a6e2ff25df36fe2
rramprasad <r.ranganathan@lakeba.com>
2018-03-15 23:53:58 +0530

    Integrated AppCenter CI

--- app/src/main/java/com/rramprasad/foodrecipes/MainActivity.kt
+++ app/src/main/java/com/rramprasad/foodrecipes/MainActivity.kt
@@ -2,11 +2,20 @@ package com.rramprasad.foodrecipes
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //AppCenter CI integration
+        AppCenter.start(application, "f9ee3e66-ebdb-4063-8b5e-816fd65a72cd",
+                Analytics::class.java, Crashes::class.java)
     }
 }
